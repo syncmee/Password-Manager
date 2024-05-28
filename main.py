3d2117ec3cad90ef6fb0b98600fc9cb353834724
syncmee <rtpurpose123@gmail.com>
2024-05-28 15:51:55 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
 from tkinter import *
 from random import *
 from tkinter import messagebox
+from json import *
 import pyperclip
 
 #password-generator
@@ -37,18 +39,38 @@ def save():
     web = website_entry.get()
     mail = mail_entry.get()
     pw = password_entry.get()
-
+    data_info = {
+        web: {
+            "email": mail,
+            "password":pw
+        }
+    }
     if len(web) == 0 or len(mail) == 0 or len(pw) == 0:
         messagebox.showerror(title="Error",message="Fields are empty. "
                                                    "\nPlease fill all the details before saving.")
     else:
         isok = messagebox.askokcancel(title=web,message=f"These are the details entered \nEmail:{mail} \nPassword:{pw} "                                               f"\nDo you want to continue?")
         if isok:
-            with open("data.txt", mode="a") as data:
-                data.writelines(f"{web} | {mail} | {pw}")
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+                data.update(data_info)
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file,indent=4)
+
             website_entry.delete(0, 'end')
             mail_entry.delete(0, 'end')
             password_entry.delete(0, 'end')
+def search():
+    with open("data.json", mode="r") as data_file:
+        data = json.load(data_file)
+        web = website_entry.get()
+        x = data[web]
+        mail = x["email"]
+        pw = x["password"]
+
+    web = website_entry.get()
+    messagebox.showinfo(title=web, message=f"Email:{mail} \nPassword:{pw}")
+
 
 
 
@@ -78,16 +100,18 @@ password.grid(column=0,row=3)
 ### Buttons
 generate = Button(text="Generate Password",command=generatepass)
 generate.grid(column=2, row=3)
-add = Button(text="Add",width=36,command=save)
+add = Button(text="Add",width=35,command=save)
 add.grid(column=1, row=4, columnspan=2)
+search = Button(text="Search",width=15,command=search)
+search.grid(column=2,row=1)
 
 ### Text-Inputs
-website_entry = Entry(width=35)
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry = Entry(width=22)
+website_entry.grid(row=1,column=1)
 website_entry.focus()
-mail_entry = Entry(width=35)
+mail_entry = Entry(width=41)
 mail_entry.grid(row=2,column=1,columnspan=2)
-password_entry = Entry(width=21)
+password_entry = Entry(width=22)
 password_entry.grid(row=3,column=1)
 
 
